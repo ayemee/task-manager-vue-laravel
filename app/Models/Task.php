1a66ab1c490f7d768c6c9c4c5e5d49dae3493481
Aimee <aimee.o.ardines@gmail.com>
2025-06-28 05:30:55 +0800

    Update Task.php

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -7,19 +7,4 @@ use Illuminate\Database\Eloquent\Model;
 class Task extends Model
 {
    protected $fillable = ['title', 'description', 'status', 'priority', 'order', 'user_id'];
-
-   // public function scopeStatus($query, $status)
-   // {
-   //    return $status ? $query->where('status', $status) : $query;
-   // }
-
-   // public function scopePriority($query, $priority)
-   // {
-   //    return $priority ? $query->where('priority', $priority) : $query;
-   // }
-
-   // public function user()
-   // {
-   //    return $this->belongsTo(User::class);
-   // }
 }
